cca3e090509a7831bd7545994bf20f4fe972ff1a
Ipaye Alameen <Akinkunmi007@gmail.com>
2020-06-27 17:51:07 +0100

    Feat: Add routing functionality

--- App.js
+++ App.js
@@ -1,21 +1,88 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+import { NavigationContainer, DrawerActions } from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { createStackNavigator } from '@react-navigation/stack'
+import { Feather } from '@expo/vector-icons'
+
+import CreateCard from './src/screens/CreateCard'
+import CreateDeck from './src/screens/CreateDeck'
+import Decks from './src/screens/Deck'
+import DeckDetail from './src/screens/DeckDetail'
+import Quiz from './src/screens/Quiz'
+
+const Drawer = createDrawerNavigator()
+const Stacks = createStackNavigator()
+
+class App extends React.Component {
+  createHomeStack = () => (
+    <Stacks.Navigator>
+      <Stacks.Screen
+        name="Home"
+        children={this.createDrawer}
+        options={({ navigation }) => ({
+          title: 'React Navigation',
+          headerLeft: () => (
+            <Feather
+              onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+              style={[{ color: 'black', marginLeft: 14 }]}
+              size={24}
+              name={'menu'}
+            />
+          ),
+        })}
+      />
+      <Stacks.Screen
+        name="Detail"
+        component={DeckDetail}
+        options={{
+          title: 'Detail Screen',
+        }}
+      />
+      <Stacks.Screen
+        name="Quiz"
+        component={Quiz}
+        options={{
+          title: 'Quiz Screen',
+        }}
+      />
+      <Stacks.Screen
+        name="CreateCard"
+        component={CreateCard}
+        options={{
+          title: 'Card Screen',
+        }}
+      />
+    </Stacks.Navigator>
   )
+
+  createDrawer = () => (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Decks" component={Decks} />
+      <Drawer.Screen name="Create Deck" component={CreateDeck} />
+    </Drawer.Navigator>
+  )
+
+  stackNavigation = () => {
+    return (
+      <Stacks.Navigator>
+        <Stacks.Screen name="Deck-Details" component={DeckDetail} />
+        <Stacks.Screen name="Quiz" component={Quiz} />
+        <Stacks.Screen name="Create Card" component={CreateCard} />
+      </Stacks.Navigator>
+    )
+  }
+
+  render() {
+    return (
+      <>
+        <StatusBar style="auto" />
+        <NavigationContainer>{this.createHomeStack()}</NavigationContainer>
+      </>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+export default App
