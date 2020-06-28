f67c0c4ad9832173f0e521d86e15e9c6f207d2f9
Ipaye Alameen <Akinkunmi007@gmail.com>
2020-06-28 20:27:11 +0100

    Feat: finish up create Card function

--- src/screens/CreateCard.js
+++ src/screens/CreateCard.js
@@ -10,23 +10,76 @@ import {
   TouchableOpacity,
   Keyboard,
 } from 'react-native'
+import { addCardToDeck, getDecks } from '../utils/api'
 
 class CreateCard extends React.Component {
+  state = {
+    question: '',
+    answer: '',
+  }
+
+  handleQuestionChange = (newValue) => {
+    this.setState({ question: newValue })
+  }
+
+  handleaAnswerChange = (newValue) => {
+    this.setState({ answer: newValue })
+  }
+
+  handleSubmit = async () => {
+    const { title } = this.props.route.params
+    const { question, answer } = this.state
+
+    console.log('title :>> ', title)
+
+    let questionData = {
+      question,
+      answer,
+    }
+
+    try {
+      await addCardToDeck(title, questionData)
+      this.props.navigation.pop()
+    } catch (error) {
+      console.log('error updating deck :>> ', error)
+    }
+  }
   render() {
+    const { question, answer } = this.state
+    let disabled
+
+    if (!question || !answer) {
+      disabled = true
+    } else {
+      disabled = false
+    }
+
     return (
       <KeyboardAvoidingView behavior={Platform.OS == 'ios' ? 'padding' : 'height'} style={styles.container}>
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
           <View style={styles.inner}>
             <View>
               <Text style={styles.header}>Question</Text>
-              <TextInput placeholder="Enter the question" style={styles.textInput} />
+              <TextInput
+                onChangeText={this.handleQuestionChange}
+                placeholder="Enter the question"
+                style={styles.textInput}
+              />
             </View>
             <View>
               <Text style={styles.header}>Card Name</Text>
-              <TextInput placeholder="Enter the answer to the question" style={styles.textInput} />
+              <TextInput
+                onChangeText={this.handleaAnswerChange}
+                placeholder="Enter the answer to the question"
+                style={styles.textInput}
+              />
             </View>
             <View style={styles.btnContainer}>
-              <TouchableOpacity style={styles.button} onPress={() => null}>
+              <TouchableOpacity
+                disabled={disabled}
+                style={[disabled ? styles.disabled : styles.button]}
+                onPress={this.handleSubmit}
+              >
                 <Text style={styles.buttonText}> Create Card</Text>
               </TouchableOpacity>
             </View>
@@ -53,6 +106,14 @@ const styles = StyleSheet.create({
     borderBottomColor: 'black',
     borderBottomWidth: 1,
     marginBottom: 20,
+  },
+  disabled: {
+    flex: 1,
+    backgroundColor: 'grey',
+    marginBottom: 10,
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   button: {
     flex: 1,
