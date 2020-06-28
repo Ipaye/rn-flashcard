4a8c5241a93d0bbdd2c096422398def9b1b26060
Ipaye Alameen <Akinkunmi007@gmail.com>
2020-06-28 20:26:26 +0100

    Feat: updated the api functions

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,24 +1,30 @@
 import { AsyncStorage } from 'react-native'
 import { formatDecks, DECKS_STORAGE_KEY } from './_decks'
 
-export function fetchDeckInformations() {
+export function getDecks() {
   return AsyncStorage.getItem(DECKS_STORAGE_KEY).then(formatDecks)
 }
 
-export function createDeck({ entry, key }) {
-  return AsyncStorage.mergeItem(
-    DECKS_STORAGE_KEY,
-    JSON.stringify({
-      [key]: entry,
-    })
-  )
+export function saveDeckTitle(deck) {
+  return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify(deck))
 }
 
-export function removeEntry(key) {
+export function addCardToDeck(deck, question) {
   return AsyncStorage.getItem(DECKS_STORAGE_KEY).then((results) => {
-    const data = JSON.parse(results)
-    data[key] = undefined
-    delete data[key]
-    AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(data))
+    const decks = JSON.parse(results)
+    let currentDeck = decks[deck]
+    currentDeck.questions.push(question)
+    AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(decks))
   })
+}
+
+export async function getDeck(deckTitle) {
+  try {
+    let results = await AsyncStorage.getItem(DECKS_STORAGE_KEY)
+    const decks = JSON.parse(results)
+    let currentDeck = decks[deckTitle]
+    return currentDeck
+  } catch (error) {
+    console.log('error :>> ', error)
+  }
 }
